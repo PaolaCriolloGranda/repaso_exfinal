6ed40b395297a208a21db1f9f6bac16ee50c3665
“Paola <priscila.criollo.est@tecazuay.edu.ec>
2022-10-05 22:46:12 -0500

    clase cliente y clase detalle

--- src/main/java/com/example/repaso_exfinal/model/Cliente.java
+++ src/main/java/com/example/repaso_exfinal/model/Cliente.java
@@ -1,4 +1,23 @@
 package com.example.repaso_exfinal.model;
 
+import lombok.*;
+
+@Setter
+@Getter
+@Data
 public class Cliente {
+    private Long cliente_id;
+    private String nombre;
+    private String primer_apellido;
+    private String segundo_apellido;
+
+    public Cliente(Long cliente_id, String nombre, String primer_apellido, String segundo_apellido) {
+        this.cliente_id = cliente_id;
+        this.nombre = nombre;
+        this.primer_apellido = primer_apellido;
+        this.segundo_apellido = segundo_apellido;
+    }
+
+    public Cliente() {
+    }
 }
---
+++ src/main/java/com/example/repaso_exfinal/model/Detalle.java
@@ -0,0 +1,15 @@
+package com.example.repaso_exfinal.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Data
+public class Detalle {
+
+    private Long pedido_id;
+    private Long producto_id;
+
+}
